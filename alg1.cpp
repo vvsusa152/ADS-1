4c381ba3a5dcb1f5a89e58bb015b1186c99d2073
vvsusa152 <62614591+vvsusa152@users.noreply.github.com>
2020-04-18 15:58:42 +0300

    Create alg1.cpp

---
+++ alg1.cpp
@@ -0,0 +1,29 @@
+int cbinsearch(int* arr, int size, int value) { 
+int first = 0; 
+int last = size - 1; 
+int search = 0; 
+while (first <= last) { 
+int mid = first + (last - first) / 2; 
+if (arr[mid] == value) { 
+search = search + 1; 
+int k = 1; 
+while ((arr[mid + k]) == value) { 
+search = search + 1; 
+k++; 
+} 
+k = 1; 
+while ((arr[mid - k]) == value) { 
+search = search + 1; 
+k++; 
+} 
+
+return search; 
+} 
+
+if (arr[mid] < value) 
+first = mid + 1; 
+else last = mid - 1; 
+} 
+
+return search; 
+}
